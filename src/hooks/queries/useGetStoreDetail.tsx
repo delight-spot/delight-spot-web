b0c6df9e7789faa028e3f555672574bb1f502c8a
TAEHEE <jangth0655@gmail.com>
2024-06-07 00:34:43 +0900

    storeDetail api, hook return type & next rewrite 업데이트

--- src/hooks/queries/useGetStoreDetail.tsx
+++ src/hooks/queries/useGetStoreDetail.tsx
@@ -1,12 +1,15 @@
-import { queryKeys } from '@/constants';
+import { number, queryKeys } from '@/constants';
 import { getStoreDetail } from '@/services/store/store';
 import { UseQueryCustomOption } from '@/types/common';
+import { StoreDetail } from '@/types/domain';
 import { useQuery } from '@tanstack/react-query';
 
-function useGetStoreDetail(id: number, queryOptions?: UseQueryCustomOption) {
+function useGetStoreDetail(id: number, queryOptions?: UseQueryCustomOption<StoreDetail>) {
   return useQuery({
     queryKey: [queryKeys.GET_STORE_DETAIL],
     queryFn: () => getStoreDetail(id),
+    staleTime: number.QUERY_ONE_HOUR_TIMES,
+    gcTime: number.QUERY_ONE_HOUR_TIMES,
     ...queryOptions,
   });
 }
--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,5 +1,5 @@
 import api from '../httpClient';
-import { Store } from '@/types/domain';
+import { Store, StoreDetail } from '@/types/domain';
 
 const getStores = async (page: number = 1, type?: string): Promise<Store[]> => {
   const data = await (
@@ -13,7 +13,7 @@ const getStores = async (page: number = 1, type?: string): Promise<Store[]> => {
   return data;
 };
 
-const getStoreDetail = async (id: number) => {
+const getStoreDetail = async (id: number): Promise<StoreDetail> => {
   const result = await (await api.get(`/stores/${id}`)).data;
   return result;
 };
