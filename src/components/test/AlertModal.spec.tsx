17c3ad8e20467098c09af751897f98383b2de46c
TAEHEE <jangth0655@gmail.com>
2024-07-15 00:56:35 +0900

    AlertModal test code 업데이트

--- src/components/test/AlertModal.spec.tsx
+++ src/components/test/AlertModal.spec.tsx
@@ -13,9 +13,17 @@ jest.mock('next/navigation', () => ({
   },
 }));
 
-it('isOpen가 true라면 렌더링된다.', async () => {
+it('모달이 열릴 때 확인 버튼이 노출된다.', async () => {
   await render(<AlertModal isOpen close={closeFn} type="error" />);
-  expect(screen.getByLabelText('alert-modal')).toBeInTheDocument();
+  const closeButton = screen.getByText('확인');
+  expect(closeButton).toBeInTheDocument();
+});
+
+it('확인 버튼을 클릭하면 onClose함수가 호출된다.', async () => {
+  const { user } = await render(<AlertModal isOpen close={closeFn} type="error" />);
+  const closeButton = screen.getByText('확인');
+  await user.click(closeButton);
+  expect(closeFn).toHaveBeenCalled();
 });
 
 it('title을 지정하면 노출된다.', async () => {
@@ -32,15 +40,6 @@ it('title이 없다면 "서비스 장애가 발생하였습니다."를 노출한
 it('description이 있다면 description을 노출한다,', async () => {
   await render(<AlertModal isOpen close={closeFn} type="error" description="test-modal-description" />);
   expect(screen.getByText('test-modal-description'));
-});
-
-it('지정한 backUrl이 있다면 url로 이동한다.', async () => {
-  const { user } = await render(<AlertModal isOpen close={closeFn} type="error" backUrl="/test" />);
-
-  const closeButton = screen.getByRole('button');
-  await user.click(closeButton);
-
-  expect(routerReplaceFn).toHaveBeenCalledWith('/test');
 });
 
 it('지정한 backUrl이 있다면 url로 이동한다.', async () => {
