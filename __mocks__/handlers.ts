fcec534fb204a594b9c38b11f531427abbefe83b
TAEHEE <jangth0655@gmail.com>
2024-07-22 17:07:43 +0900

    msw api handlers - reivew list 업데이트

--- __mocks__/handlers.ts
+++ __mocks__/handlers.ts
@@ -1,5 +1,6 @@
 import { rest } from 'msw';
 import storeData from '../src/server/response/stores.json';
+import reviewList from '../src/server/response/reviews.json';
 import StoreDetailData from '../src/server/response/storeDetail.json';
 
 const API_BASE_URL = '/api/v1';
@@ -9,7 +10,7 @@ export const handlers = [
   rest.get(`${API_BASE_URL}/stores`, (req, res, ctx) => {
     const type = req.url.searchParams.get('type') ?? 'all';
     const page = parseInt(req.url.searchParams.get('page') ?? '1', 10);
-    const startIndex = 0;
+    const startIndex = (page - 1) * pageSize;
     const endIndex = page * pageSize;
     const storeList = storeData.slice(startIndex, endIndex).filter((item) => {
       if (type === 'all') return item;
@@ -28,7 +29,11 @@ export const handlers = [
   }),
 
   rest.get(`${API_BASE_URL}/stores/:id/reviews`, (req, res, ctx) => {
-    return res(ctx.status(200));
+    const page = parseInt(req.url.searchParams.get('page') ?? '1', 10);
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = page * pageSize;
+    const reviewData = reviewList.slice(startIndex, endIndex);
+    return res(ctx.status(200), ctx.json(reviewData));
   }),
 
   rest.get(`${API_BASE_URL}/users/me`, (req, res, ctx) => {
