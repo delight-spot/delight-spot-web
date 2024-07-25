47868d6f7f10cb492c574220132e69d18786dbf1
TAEHEE <jangth0655@gmail.com>
2024-07-25 16:04:35 +0900

    찜목록 페이지 - BookingStoreList

--- src/app/my/bookings/page.tsx
+++ src/app/my/bookings/page.tsx
@@ -1,3 +1,12 @@
-export default function BookingStoresPage() {
-  return <section>booking stores</section>;
+import Header from '@/components/header/Header';
+import HeaderMenu from '@/components/HeaderMenu';
+import BookingStoreList from '@/components/stores/BookingStoreList';
+
+export default function BookingStorePage() {
+  return (
+    <section>
+      <Header isBack title="찜 스토어" customMenu={<HeaderMenu />} />
+      <BookingStoreList />
+    </section>
+  );
 }
